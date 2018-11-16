e1e09bbf2b249ae8b02af431a6d7e9b6ad40c2d3
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2018-11-16 18:41:40 +0100

    Added protox app & django_extensions for ERD in settings.
    - registered models in admin.py

--- prottox/admin.py
+++ prottox/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from prottox.models import *
 
-# Register your models here.
+admin.site.register(Toxin)
+admin.site.register(Target)
+admin.site.register(Target_organism_name)
+admin.site.register(Toxin_research)
+admin.site.register(Publication)
+admin.site.register(Author)
+admin.site.register(Larvae_stage)
+admin.site.register(Toxin_distribution)
+admin.site.register(Toxin_source)
+admin.site.register(Toxin_form)
+admin.site.register(Toxin_isolation_source)
+admin.site.register(Toxin_expression_host)
+admin.site.register(Measurement)
+admin.site.register(Bioassay_type)
+admin.site.register(Results)
+admin.site.register(Toxin_quantity)
+admin.site.register(Family_rank_1)
+admin.site.register(Family_rank_2)
+admin.site.register(Family_rank_3)
+admin.site.register(Family_rank_4)
