2fa7121ef4896b8527f20201b705dde3986846d5
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2019-07-21 20:53:41 +0200

    Add datatable columns visibility control
    Added multiselect dropdown to select columns that should be visible

--- api/views.py
+++ api/views.py
@@ -139,7 +139,8 @@ def __processDatatableToxinResearchToJSON(queryset):
         entry = {}
         entry['title'] = name
         entry['data'] = name
-        #entry['visible'] = name in DATATABLE_VISIBLE_COLUMNS
+        entry['visible'] = name in DATATABLE_VISIBLE_COLUMNS
+        entry['name'] = name
 
         json['columns'].append(entry)
 
--- prottox/static/prottox/js/research_browser.js
+++ prottox/static/prottox/js/research_browser.js
@@ -1,5 +1,8 @@
+"use strict"
+var DBdata = null;
+
 function initDataTable(json){
-    $('#table').dataTable({
+    $('#table').DataTable({
         data:json['data'],
         columns:json['columns'],
         colReorder: true,
@@ -9,17 +12,35 @@ function initDataTable(json){
 
 }
 
+function initColumnsFilter(json){
+    let selected = [];
+
+    for (const column of json['columns']) {
+        if(column.visible === true){
+            selected.push(column.title)
+        }
+        var options = "<option " + "value='" + column.title + "'>" + column.title + "</option>";
+        $("#columnFilter").append(options);
+    }
+
+    $("#columnFilter").selectpicker('val', selected);
+    $("#columnFilter").selectpicker('refresh');
+}
+
+
+  
+
+
 $(document).ready(function () {
 
     var ids = JSON.parse(document.getElementById('IDs').textContent);
     var db = JSON.parse(document.getElementById('DB').textContent);
     var API_ADDR = '/api/researchbrowser?ids='+ids+'&db='+db;
-
     
     $.ajax( {
         url: API_ADDR,
         beforeSend: function(){
-            KTApp.block('#research_table', {
+            KTApp.blockPage({
                 type: 'v2',
                 state: 'success',
                 size: 'lg',
@@ -27,10 +48,24 @@ $(document).ready(function () {
             });
         },
         success: function ( json ) {
+            DBdata = json;
             initDataTable(json);
+            initColumnsFilter(json);
+
         },
         complete: function(){
-            KTApp.unblock('#research_table');
+            KTApp.unblockPage();
+            $('#columnFilter').on('change', function (){
+                // Get the column API object
+                let selected = [];
+                selected = $('#columnFilter').val()
+                $("#table").DataTable().columns().visible(false);
+                for (const columnName of selected) {
+                    let column = $("#table").DataTable().column(columnName+':name');
+                    column.visible(true);
+                }
+            
+              });
         },
         dataType: "json"
     } );
